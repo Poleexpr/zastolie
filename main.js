0f3355456eca045be1f6dd5e7287751fc619e65d
Anya Cherkasova <a.c4erkasova@gmail.com>
2023-12-24 12:56:18 +0300

    change mode

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+/*import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+*/
 
-/*
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-*/
+
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
